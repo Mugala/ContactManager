95cd158fe3a154b87f454916ca0d83414b593733
mugala <mugalalevi21@gmail.com>
2018-10-03 15:25:08 +0300

    Styling in the Contact.js This commit will implement the defferent types of styling in React

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
   const { branding } = props;
   return (
     <div>
-      <h1>{branding}</h1>
+      <h1 style={headingStyle}>{branding}</h1>
     </div>
   );
 };
@@ -18,5 +18,10 @@ Header.defaultProps = {
 Header.propTypes = {
   branding: PropTypes.string.isRequired
 };
+
+const headingStyle = {
+  color: 'green', 
+  fontSize: '50px'
+}
 
 export default Header;
