449a2b4a16ff1ea09fba9e2573bb8533a32f27c5
mugala <mugalalevi21@gmail.com>
2018-10-03 14:30:59 +0300

    Import Contact This commit will import the contact component to app.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,11 @@ class Contact extends Component {
   render() {
     return (
       <div>
-        
+        <h4>John Doe</h4>
+        <ul>
+          <li>Email: jdoe@gmail.com</li>
+          <li>Phone: 555-555-5555</li>
+        </ul>
       </div>
     )
   }
