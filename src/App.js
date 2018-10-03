054df19655a1074810b2b1a2cb1c5c88f3f6568a
mugala <mugalalevi21@gmail.com>
2018-10-03 15:07:37 +0300

    Props and destructuring This commit will implement props on both class and functional component and destructure them.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
+import Header from './components/Header';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>The App Component</h1>
-       <Contact/>
+        <Header branding ="Contact manager"/>
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555"/>
+        <Contact name="Karen smith" email="karen@gmail.com" phone="333-333-3333"/>
       </div>
     );
   }
