8c3eda819149a626b6d4faf4aa36f6b32004c4c6
mugala <mugalalevi21@gmail.com>
2018-10-04 14:27:13 +0300

    Toggle the ul This commit will change the state and unable users to goggle the ul with email and phone

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,24 +4,22 @@ import PropTypes from 'prop-types';
 
 class Contact extends Component {
   state = {
-    showContactInfo: true
-  };
-
-  onShowClick = (e) => {
-    this.setState({showContactInfo: !this.state.showContactInfo});
+    showContactInfo: false
   };
 
   render() {
     const {name, email, phone} = this.props.contact;
+    const { showContactInfo } =this.state;
 
     return (
       <div className="card card-body mb-3">
-        <h4>{name} <i onClick={this.onShowClick} 
+        <h4>{name} <i onClick={()=>this.setState({showContactInfo: !this.state.showContactInfo})} 
         className="fas fa-sort-down"></i> </h4>
-        <ul className="list-group">
+        {showContactInfo ? (<ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone:{phone}</li>
-        </ul>
+        </ul>) : null }
+        
       </div>
     );
   }
