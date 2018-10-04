5e0b9d2ca1e1dffb213f58236bdd3ceb793de566
mugala <mugalalevi21@gmail.com>
2018-10-04 13:24:01 +0300

    onShowClick This commit will create the event handler or onshowclick

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 
 
 class Contact extends Component {
+  state = {};
+
+  onShowClick = (e) => {
+   
+  };
+
   render() {
     const {name, email, phone} = this.props.contact;
 
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>{name} <i onClick={this.onShowClick} 
+        className="fas fa-sort-down"></i> </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone:{phone}</li>
@@ -19,7 +26,7 @@ class Contact extends Component {
 }
 
 Contact.propTypes ={
-  contact: PropTypes.object .isRequired,
+  contact: PropTypes.object.isRequired,
 }
 
 
