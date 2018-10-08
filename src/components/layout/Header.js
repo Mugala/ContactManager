8d28ae2270a9bf857fe4ed111a2d9ce398fbc56b
mugala <mugalalevi21@gmail.com>
2018-10-08 15:39:22 +0300

    Links to About and Add This commi will add link to about and add contact pages

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 
 const Header = (props) => {
@@ -11,7 +12,11 @@ const Header = (props) => {
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-            <a href="/" className="nav-link">Home</a></li>
+            <Link to="/" className="nav-link">Home</Link></li>
+            <li className="nav-item">
+            <Link to="/contact/add" className="nav-link">Add</Link></li>
+            <li className="nav-item">
+            <Link to="/about" className="nav-link">About</Link></li>
           </ul>
         </div>
       </div>
