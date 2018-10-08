622c7e9ee081b9b7b9d483a13fbf3caca0ee752a
mugala <mugalalevi21@gmail.com>
2018-10-08 15:29:00 +0300

    Contacts Router and Abouts page This commit will build the Contacts router and the abouts.js which is a static component file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import AddContact from './components/contacts/AddContact';
@@ -6,19 +7,22 @@ import AddContact from './components/contacts/AddContact';
 import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './App.css';  
 
 class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding ="Contact manager"/>
-          <div className="container">
-            <AddContact/>
-            <Contacts />
+          <Router>
+          <div className="App">
+            <Header branding ="Contact manager"/>
+            <div className="container">
+              <Switch>
+                <Route exact path='/' component={Contacts}/>
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
