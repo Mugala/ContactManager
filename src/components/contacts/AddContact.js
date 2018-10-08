ce9d350db93fff3e77d9470f65d102ff93b78a92
mugala <mugalalevi21@gmail.com>
2018-10-08 13:39:44 +0300

    Validation This commit will addthe validation on the UI

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -20,14 +20,17 @@ class AddContact extends Component {
     // check For Errors
     if(name === ''){
       this.setState({errors: {name: 'Name is Required'}});
+      return;
     }
 
     if(email === ''){
       this.setState({errors: {email: 'Email is Required'}});
+      return
     }
 
     if(phone === ''){
       this.setState({errors: {phone: 'Phone is Required'}});
+      return;
     }
 
     const newContact = {
