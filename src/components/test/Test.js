a7cbd7eef87db27afb10413412c9fe6779e2483e
mugala <mugalalevi21@gmail.com>
2018-10-09 12:49:13 +0300

    Get data using fetch This commit will get data from jsonplaceholder using fetch api

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,40 +2,51 @@ import React, { Component } from 'react'
 
 class Test extends Component {
   state = {
-    test: 'test'
+    title: '',
+    body:''
+
   };
 
   componentDidMount(){
-    console.log('Component did mount...');
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(data => this.setState({
+        title: data.id,
+        body: data.title
+      })
+      );
   }
 
-  componentWillMount(){
-    console.log('Component will mount');
-  }
-  componentDidUpdate(){
-    console.log('Component Did update...');
-  }
+  // componentWillMount(){
+  //   console.log('Component will mount');
+  // }
+  // componentDidUpdate(){
+  //   console.log('Component Did update...');
+  // }
 
-  componentWillUpdate(){
-    console.log('Component will update...');
-  }
+  // componentWillUpdate(){
+  //   console.log('Component will update...');
+  // }
 
-  componentWillReceiveProps(nextProps, nextState){
-    console.log('ComponentWillReceiveProps');
-  }
+  // componentWillReceiveProps(nextProps, nextState){
+  //   console.log('ComponentWillReceiveProps');
+  // }
 
-  static getDerivedStateFromProps(nextProps, prevState){
-    return null;
-  }
+  // static getDerivedStateFromProps(nextProps, prevState){
+  //   return null;
+  // }
 
-  getSnapshotBeforeUpdate(prevProps, prevState){
-    console.log('getSnapshotBeforeUpdate');
-  }
+  // getSnapshotBeforeUpdate(prevProps, prevState){
+  //   console.log('getSnapshotBeforeUpdate');
+  // }
 
   render() {
+    const {title, body } = this.state;
+
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
