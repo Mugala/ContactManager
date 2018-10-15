4f05b1eaef47504875ce021750ca0438beedae4b
mugala <mugalalevi21@gmail.com>
2018-10-15 15:11:51 +0300

    Updates

---
+++ precache-manifest.0fce481a50fa975d71f5d02150ef9882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3b43f1fb5c677b2d034",
+    "url": "/Mugala/contactManager/static/js/runtime~main.a3b43f1f.js"
+  },
+  {
+    "revision": "df878a9a62ed8c7be54a",
+    "url": "/Mugala/contactManager/static/js/main.df878a9a.chunk.js"
+  },
+  {
+    "revision": "0301d41f2c93392d8380",
+    "url": "/Mugala/contactManager/static/js/1.0301d41f.chunk.js"
+  },
+  {
+    "revision": "df878a9a62ed8c7be54a",
+    "url": "/Mugala/contactManager/static/css/main.f0d23b66.chunk.css"
+  },
+  {
+    "revision": "0301d41f2c93392d8380",
+    "url": "/Mugala/contactManager/static/css/1.8f55fb31.chunk.css"
+  },
+  {
+    "revision": "3149d53f26aa86f112eecbb772ef4e52",
+    "url": "/Mugala/contactManager/index.html"
+  }
+];
